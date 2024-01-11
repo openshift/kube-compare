90875c326357ff437c0d58f187506ab7ab2888d0
Alina Sudakov <asudakov@redhat.com>
2024-01-11 14:30:13 +0200

    Add compare command
    This commit introduces a new command that intelligently diffs a known valid
    reference configuration against a set of specific cluster configuration
    resources. The command matches each resource in the cluster configuration with
    a template in the reference configuration, injects the templated resource with
    cluster resource parameters, and presents users with a detailed diff
    highlighting differences between each cluster resource and its corresponding
    injected template. This command supports in validating adaptations of
    configurations against the original reference, identifying and addressing
    potential issues.
    
    Signed-off-by: Alina Sudakov <asudakov@redhat.com>

---
+++ pkg/compare/compare.go
@@ -0,0 +1,471 @@
+package compare
+
+import (
+	"bytes"
+	"fmt"
+	"os"
+	"path/filepath"
+	"sort"
+	"strings"
+	"text/template"
+
+	"github.com/gosimple/slug"
+	"github.com/openshift/oc/pkg/compare/groups"
+	"github.com/samber/lo"
+	"github.com/spf13/cobra"
+	"k8s.io/apimachinery/pkg/apis/meta/v1/unstructured"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/cli-runtime/pkg/genericiooptions"
+	"k8s.io/cli-runtime/pkg/resource"
+	"k8s.io/client-go/discovery"
+	"k8s.io/klog/v2"
+	"k8s.io/kubectl/pkg/cmd/diff"
+	kcmdutil "k8s.io/kubectl/pkg/cmd/util"
+	"k8s.io/kubectl/pkg/util/i18n"
+	"k8s.io/kubectl/pkg/util/templates"
+	"k8s.io/utils/exec"
+)
+
+var (
+	compareLong = templates.LongDesc(`
+		Compare a known valid reference configuration and a set of specific cluster configuration CRs.
+		
+		The reference configuration consists of Resource templates. 
+		Resource Templates are files that contain Resource definitions and with fixed and optional content. Optional content is represented as Go templates.
+		The compare command will match each Resource in the cluster configuration to a Resource Template in the reference 
+		configuration. Then, the templated Resource will be injected with the cluster Resource parameters. 
+		For each cluster Resource, a diff between the Resource and its matching injected template will be presented
+    	to the user.
+		
+		The input cluster configuration may be provided as an "offline" set of CRs or can be pulled from a live cluster.
+		
+		The Reference also includes a mandatory metadata.yaml file where all the Resource templates should be specified.
+		The Resource templates can be divided into components. Each component and Resource template can be set as required,
+		resulting in a report to the user in case one of them is missing.
+		
+		Each Resource definition should be in its own template file. 
+		The input to the Go template is the "input cluster configuration" in order to allow expected user variable content
+		to be synchronized between cluster CR and reference CR prior to the diff.
+		The usage of all Go built-in functions is supported along with the functions in the Sprig library.
+		All templates should always be valid YAML after template execution, even when injecting an empty mapping.
+		Before using functions that can fail for nil values, always check that the value exists.
+
+		It's possible to pass a user config that contains an option to specify manual matches between cluster resources
+		and Resource templates. The matches can be added to the config as pairs of 
+		apiVersion_kind_namespace_name: <Template File Name>. For resources that don't have a namespace the matches can 
+		be added  as pairs of apiVersion_kind_name: <Template File Name>.
+
+		KUBECTL_EXTERNAL_DIFF environment variable can be used to select your own diff
+		command. Users can use external commands with params too, example:
+		KUBECTL_EXTERNAL_DIFF="colordiff -N -u"
+		
+		 By default, the "diff" command available in your path will be run with the "-u"
+		(unified diff) and "-N" (treat absent files as empty) options.
+		
+		 Exit status: 0 No differences were found. 1 Differences were found. >1 oc
+		or diff failed with an error.
+		
+		 Note: KUBECTL_EXTERNAL_DIFF, if used, is expected to follow that convention.
+
+		Experimental: This command is under active development and may change without notice.
+	`)
+
+	compareExample = templates.Examples(`
+		# Compare a known valid reference configuration with a live cluster:
+		oc adm compare -r ./reference
+		
+		# Compare a known valid reference configuration with a local set of CRs:
+		oc adm compare -r ./reference -f ./crsdir -R
+
+		# Compare a known valid reference configuration with a live cluster and with a user config:
+		oc adm compare -r ./reference -c ./user_config
+
+		# Run a known valid reference configuration with a must-gather output:
+		oc adm compare -r ./reference -f "must-gather*/*/cluster-scoped-resources","must-gather*/*/namespaces" -R
+	`)
+)
+
+const (
+	ReferenceFileName        = "metadata.yaml"
+	noReffDirectoryWasPassed = "\"Reference directory is required\""
+	reffDirNotExistsError    = "\"Reference directory doesnt exist\""
+	emptyTypes               = "Templates don't contain any types (kind) of resources that are supported by the cluster"
+)
+
+type Options struct {
+	CRs                resource.FilenameOptions
+	templatesDir       string
+	diffConfigFileName string
+	diffAll            bool
+
+	builder     *resource.Builder
+	corelator   *MetricsCorelatorDecorator
+	templates   []*template.Template
+	local       bool
+	types       []string
+	reff        Reference
+	userConfig  UserConfig
+	Concurrency int
+
+	diff *diff.DiffProgram
+	genericiooptions.IOStreams
+}
+
+func NewCmd(f kcmdutil.Factory, streams genericiooptions.IOStreams) *cobra.Command {
+	options := NewOptions(streams)
+	cmd := &cobra.Command{
+		Use:                   "compare -r <Reference Directory>",
+		DisableFlagsInUseLine: true,
+		Short:                 i18n.T("Compare a reference configuration and a set of cluster configuration CRs."),
+		Long:                  compareLong,
+		Example:               compareExample,
+		Run: func(cmd *cobra.Command, args []string) {
+			kcmdutil.CheckDiffErr(options.Complete(f, cmd, args))
+			// `oc compare` propagates the error code from
+			//`kubectl diff` that propagates the error code from
+			// diff or `KUBECTL_EXTERNAL_DIFF`. Also, we
+			// don't want to print an error if diff returns
+			// error code 1, which simply means that changes
+			// were found. We also don't want oc to
+			// return 1 if there was a problem.
+			if err := options.Run(); err != nil {
+				if exitErr := diffError(err); exitErr != nil {
+					kcmdutil.CheckErr(kcmdutil.ErrExit)
+				}
+				kcmdutil.CheckDiffErr(err)
+			}
+		},
+	}
+
+	// Flag errors exit with code 1, however according to the diff
+	// command it means changes were found.
+	// Thus, it should return status code greater than 1.
+	cmd.SetFlagErrorFunc(func(command *cobra.Command, err error) error {
+		kcmdutil.CheckDiffErr(kcmdutil.UsageErrorf(cmd, err.Error()))
+		return nil
+	})
+
+	cmd.Flags().IntVar(&options.Concurrency, "concurrency", 4,
+		"Number of objects to process in parallel when diffing against the live version. Larger number = faster,"+
+			" but more memory, I/O and CPU over that shorter period of time.")
+	kcmdutil.AddFilenameOptionFlags(cmd, &options.CRs, "contains the configuration to diff")
+	cmd.Flags().StringVarP(&options.diffConfigFileName, "diff-config", "c", "", "Path to the user config file")
+	cmd.Flags().StringVarP(&options.templatesDir, "reference", "r", "", "Path to directory including reference.")
+	cmd.Flags().BoolVarP(&options.diffAll, "all-resources", "A", options.diffAll,
+		"If present, In live mode will try to match all resources that are from the types mentioned in the reference. "+
+			"In local mode will try to match all resources passed to the command")
+
+	return cmd
+}
+
+func NewOptions(ioStreams genericiooptions.IOStreams) *Options {
+	return &Options{
+		IOStreams: ioStreams,
+		diff: &diff.DiffProgram{
+			Exec:      exec.New(),
+			IOStreams: ioStreams,
+		},
+	}
+}
+
+// DiffError returns the ExitError if the status code is less than 1,
+// nil otherwise.
+func diffError(err error) exec.ExitError {
+	if err, ok := err.(exec.ExitError); ok && err.ExitStatus() <= 1 {
+		return err
+	}
+	return nil
+}
+
+func (o *Options) Complete(f kcmdutil.Factory, cmd *cobra.Command, args []string) error {
+	var err error
+	o.builder = f.NewBuilder()
+
+	if o.templatesDir == "" {
+		return kcmdutil.UsageErrorf(cmd, noReffDirectoryWasPassed)
+	}
+	if _, err := os.Stat(o.templatesDir); os.IsNotExist(err) {
+		return fmt.Errorf(reffDirNotExistsError)
+	}
+
+	o.reff, err = getReference(o.templatesDir)
+	if err != nil {
+		return err
+	}
+	appendDir := func(item string, index int) string {
+		return filepath.Join(o.templatesDir, item)
+	}
+
+	if o.diffConfigFileName != "" {
+		o.userConfig, err = parseDiffConfig(o.diffConfigFileName)
+		if err != nil {
+			return err
+		}
+	}
+
+	o.templates, err = getTemplates(lo.Map(o.reff.getTemplates(), appendDir), lo.Map(o.reff.TemplateFunctionFiles, func(item string, index int) string {
+		return filepath.Join(o.templatesDir, item)
+	}))
+	if err != nil {
+		return err
+	}
+
+	err = o.setupCorelators()
+	if err != nil {
+		return err
+	}
+
+	if len(args) != 0 {
+		return kcmdutil.UsageErrorf(cmd, "Unexpected args: %v", args)
+	}
+	err = o.CRs.RequireFilenameOrKustomize()
+
+	if err == nil {
+		o.local = true
+		o.types = []string{}
+		return nil
+	}
+
+	return o.setLiveSearchTypes(f)
+}
+
+// setupCorelators initializes a chain of corelators based on the provided options.
+// The correlation chain consists of base corelators wrapped with decorator corelators.
+// This function configures the following base corelators:
+//  1. ExactMatchCorelator - Matches CRs based on pairs specifying, for each cluster CR, its matching template.
+//     The pairs are read from the diff config and provided to the corelator.
+//  2. GroupCorelator - Matches CRs based on groups of fields that are similar in cluster resources and templates.
+//
+// The base corelators are combined using a MultiCorelator, which attempts to match a template for each base corelator
+// in the specified sequence. The MultiCorelator is further wrapped with a MetricsCorelatorDecorator.
+// This decorator not only correlates templates but also records metrics, allowing retrieval that then can be used to create a summary.
+func (o *Options) setupCorelators() error {
+	var corelators []Corelator
+	if len(o.userConfig.CorrelationSettings.ManualCorrelation.CorrelationPairs) > 0 {
+		manualCorelator, err := NewExactMatchCorelator(o.userConfig.CorrelationSettings.ManualCorrelation.CorrelationPairs, o.templates)
+		if err != nil {
+			return err
+		}
+		corelators = append(corelators, manualCorelator)
+	}
+
+	// These fields are used by the GroupCorelator who attempts to match templates based on the following priority order:
+	// apiVersion_name_namespace_kind. If no single match is found, it proceeds to trying matching by apiVersion_name_kind,
+	// then namespace_kind, and finally kind alone.
+	//
+	// For instance, consider a template resource with fixed apiVersion, name, and kind, but a templated namespace. The
+	// correlator will potentially match this template based on its fixed fields: apiVersion_name_kind.
+	var fieldGroups = [][][]string{
+		{{"apiVersion"}, {"metadata", "name"}, {"metadata", "namespace"}, {"kind"}},
+		{{"apiVersion"}, {"metadata", "namespace"}, {"kind"}},
+		{{"metadata", "name"}, {"metadata", "namespace"}, {"kind"}},
+		{{"apiVersion"}, {"metadata", "name"}, {"kind"}},
+		{{"metadata", "name"}, {"kind"}},
+		{{"metadata", "namespace"}, {"kind"}},
+		{{"apiVersion"}, {"kind"}},
+		{{"kind"}},
+	}
+	groupCorelator, err := NewGroupCorelator(fieldGroups, o.templates)
+	if err != nil {
+		return err
+	}
+
+	corelators = append(corelators, groupCorelator)
+
+	var erorrsToIgnore []error
+
+	if !o.diffAll {
+		erorrsToIgnore = []error{UnknownMatch{}}
+	}
+	o.corelator = NewMetricsCorelatorDecorator(NewMultiCorealtor(corelators), o.reff.Parts, erorrsToIgnore)
+	return nil
+}
+
+// setLiveSearchTypes creates a set of resources types to search the live cluster for in order to retrieve cluster resources.
+// The types are gathered from the templates included in the reference. The set of types is filtered, so it will include only
+// types supported by the live cluster in order to not raise errors by the visitor. In a case the reference includes types that
+// are not supported by the user a warning will be created.
+func (o *Options) setLiveSearchTypes(f kcmdutil.Factory) error {
+	types, err := groups.Divide(o.templates, func(element *unstructured.Unstructured) ([]int, error) {
+		return []int{0}, nil
+	}, extractMetadata, createGroupHashFunc([][]string{{"kind"}}))
+	if err != nil {
+		return err
+	}
+	c, err := f.ToDiscoveryClient()
+	resources, _ := getSupportedResourceTypes(c)
+	o.types = lo.Filter(lo.Keys(types[0]), func(x string, index int) bool {
+		_, ok := resources[x]
+		return ok
+	})
+	if len(o.types) != len(lo.Keys(types[0])) {
+		notSupportedTypes := lo.Filter(lo.Keys(types[0]), func(x string, index int) bool {
+			_, ok := resources[x]
+			return !ok
+		})
+		sort.Strings(notSupportedTypes)
+		klog.Warningf("Reference Contains Templates With Types (kind) Not Supported By Cluster: %s", strings.Join(notSupportedTypes, ", "))
+	}
+	if len(o.types) == 0 {
+		return fmt.Errorf(emptyTypes)
+	}
+	return nil
+}
+
+// setLiveSearchTypes retrieves a set of resource types that are supported by the cluster.
+func getSupportedResourceTypes(client discovery.CachedDiscoveryInterface) (map[string]bool, error) {
+	resources := make(map[string]bool)
+	lists, err := client.ServerPreferredResources()
+	if err != nil {
+		return resources, err
+	}
+	for _, list := range lists {
+		if len(list.APIResources) != 0 {
+			for _, res := range list.APIResources {
+				resources[res.Kind] = true
+			}
+		}
+	}
+	return resources, nil
+
+}
+
+// Run uses the factory to parse file arguments (in case of local mode) or gather all cluster resources matching
+// templates types. For each Resource it finds the matching Resource template and
+// injects, compares, and runs against differ.
+func (o *Options) Run() error {
+	showManagedFields := false
+
+	differ, err := diff.NewDiffer("LIVE", "MERGED")
+	if err != nil {
+		return err
+	}
+	defer differ.TearDown()
+
+	printer := diff.Printer{}
+	r := o.builder.
+		Unstructured().
+		VisitorConcurrency(o.Concurrency).
+		AllNamespaces(true).
+		LocalParam(o.local).
+		FilenameParam(false, &o.CRs).
+		ResourceTypes(o.types...).
+		SelectAllParam(!o.local).
+		ContinueOnError().
+		Flatten().
+		Do()
+	if err := r.Err(); err != nil {
+		return err
+	}
+	r.IgnoreErrors(func(err error) bool {
+		return containOnly(err, []error{MultipleMatches{}, UnknownMatch{}})
+	})
+	err = r.Visit(func(info *resource.Info, err error) error {
+		clusterCRMapping, _ := runtime.DefaultUnstructuredConverter.ToUnstructured(info.Object)
+		clusterCR := unstructured.Unstructured{Object: clusterCRMapping}
+
+		temp, err := o.corelator.Match(&clusterCR)
+		if err != nil {
+			return err
+		}
+
+		localRef, err := executeYAMLTemplate(temp, clusterCR.Object)
+		if err != nil {
+			return err
+		}
+
+		obj := InfoObject{
+			injectedObjfromTemplate: localRef,
+			clusterobj:              &clusterCR,
+			FieldsToOmit:            o.reff.FieldsToOmit,
+		}
+
+		err = differ.Diff(obj, printer, showManagedFields)
+
+		return err
+	})
+
+	if err != nil {
+		return err
+	}
+
+	err = differ.Run(o.diff)
+	differ.TearDown()
+	a := newSummary(&o.reff, o.corelator)
+	_, _ = o.Out.Write([]byte(a.String()))
+	return err
+}
+
+// InfoObject matches the diff.Object interface, it contains the objects that shall be compared.
+type InfoObject struct {
+	injectedObjfromTemplate *unstructured.Unstructured
+	clusterobj              *unstructured.Unstructured
+	FieldsToOmit            [][]string
+}
+
+// Live Returns the cluster version of the object
+func (obj InfoObject) Live() runtime.Object {
+	omitFields(obj.clusterobj.Object, obj.FieldsToOmit)
+	return obj.clusterobj
+}
+
+// Merged Returns the Injected Reference Version of the Resource
+func (obj InfoObject) Merged() (runtime.Object, error) {
+	omitFields(obj.injectedObjfromTemplate.Object, obj.FieldsToOmit)
+	return obj.injectedObjfromTemplate, nil
+}
+
+func omitFields(object map[string]any, fields [][]string) {
+	for _, field := range fields {
+		unstructured.RemoveNestedField(object, field...)
+		for i := 0; i <= len(field); i++ {
+			val, _, _ := unstructured.NestedFieldNoCopy(object, field[:len(field)-i]...)
+			if mapping, ok := val.(map[string]any); ok && len(mapping) == 0 {
+				unstructured.RemoveNestedField(object, field[:len(field)-i]...)
+			}
+		}
+	}
+	return
+}
+
+func (obj InfoObject) Name() string {
+	return slug.Make(apiKindNamespaceName(obj.clusterobj))
+}
+
+// summary Contains all info included in the summary output of the compare command
+type summary struct {
+	RequiredCRS  map[string]map[string][]string
+	NumMissing   int
+	UnmatchedCRS []string
+}
+
+func newSummary(reference *Reference, c *MetricsCorelatorDecorator) *summary {
+	s := summary{}
+	s.RequiredCRS, s.NumMissing = reference.getMissingCRs(c.MatchedTemplatesNames)
+	s.UnmatchedCRS = lo.Map(c.UnMatchedCRs, func(r *unstructured.Unstructured, i int) string {
+		return apiKindNamespaceName(r)
+	})
+	return &s
+}
+
+func (s summary) String() string {
+	t := `
+Summary
+{{- if ne (len  .RequiredCRS) 0 }}
+Missing required CRs: {{.NumMissing}} 
+{{ toYaml .RequiredCRS}}
+{{- else}}
+No CRs are missing
+{{- end }}
+{{- if ne (len  .UnmatchedCRS) 0 }}
+CRs are unmatched: {{len  .UnmatchedCRS}}
+{{ toYaml .UnmatchedCRS}}
+{{- else}}
+No CRs are unmatched
+{{- end }}
+`
+	var buf bytes.Buffer
+	tmpl, _ := template.New("summary").Funcs(template.FuncMap{"toYaml": toYAML}).Parse(t)
+	_ = tmpl.Execute(&buf, s)
+	return buf.String()
+}
---
+++ pkg/compare/funcmap.go
@@ -0,0 +1,150 @@
+package compare
+
+import (
+	"bytes"
+	"encoding/json"
+	"strings"
+	"text/template"
+
+	"github.com/BurntSushi/toml"
+	"github.com/Masterminds/sprig/v3"
+	"sigs.k8s.io/yaml"
+)
+
+// This File is almost identical to the FuncMap used in Helm to match Helm templating behaviour.
+
+// FuncMap returns a mapping of all of the functions that Engine has.
+//
+// Because some functions are late-bound (e.g. contain context-sensitive
+// data), the functions may not all perform identically outside of an Engine
+// as they will inside of an Engine.
+//
+// Known late-bound functions:
+//
+//   - "include"
+//   - "tpl"
+//
+// These are late-bound in Engine.Render().  The
+// version included in the FuncMap is a placeholder.
+func FuncMap() template.FuncMap {
+	f := sprig.TxtFuncMap()
+	delete(f, "env")
+	delete(f, "expandenv")
+
+	// Add some extra functionality
+	extra := template.FuncMap{
+		"toToml":        toTOML,
+		"toYaml":        toYAML,
+		"fromYaml":      fromYAML,
+		"fromYamlArray": fromYAMLArray,
+		"toJson":        toJSON,
+		"fromJson":      fromJSON,
+		"fromJsonArray": fromJSONArray,
+	}
+
+	for k, v := range extra {
+		f[k] = v
+	}
+
+	return f
+}
+
+// toYAML takes an interface, marshals it to yaml, and returns a string. It will
+// always return a string, even on marshal error (empty string).
+//
+// This is designed to be called from a template.
+func toYAML(v any) string {
+	data, err := yaml.Marshal(v)
+	if err != nil {
+		// Swallow errors inside of a template.
+		return ""
+	}
+	return strings.TrimSuffix(string(data), "\n")
+}
+
+// fromYAML converts a YAML document into a map[string]any.
+//
+// This is not a general-purpose YAML parser, and will not parse all valid
+// YAML documents. Additionally, because its intended use is within templates
+// it tolerates errors. It will insert the returned error message string into
+// m["Error"] in the returned map.
+func fromYAML(str string) map[string]any {
+	m := map[string]any{}
+
+	if err := yaml.Unmarshal([]byte(str), &m); err != nil {
+		m["Error"] = err.Error()
+	}
+	return m
+}
+
+// fromYAMLArray converts a YAML array into a []any.
+//
+// This is not a general-purpose YAML parser, and will not parse all valid
+// YAML documents. Additionally, because its intended use is within templates
+// it tolerates errors. It will insert the returned error message string as
+// the first and only item in the returned array.
+func fromYAMLArray(str string) []any {
+	a := []any{}
+
+	if err := yaml.Unmarshal([]byte(str), &a); err != nil {
+		a = []any{err.Error()}
+	}
+	return a
+}
+
+// toTOML takes an interface, marshals it to toml, and returns a string. It will
+// always return a string, even on marshal error (empty string).
+//
+// This is designed to be called from a template.
+func toTOML(v any) string {
+	b := bytes.NewBuffer(nil)
+	e := toml.NewEncoder(b)
+	err := e.Encode(v)
+	if err != nil {
+		return err.Error()
+	}
+	return b.String()
+}
+
+// toJSON takes an interface, marshals it to json, and returns a string. It will
+// always return a string, even on marshal error (empty string).
+//
+// This is designed to be called from a template.
+func toJSON(v any) string {
+	data, err := json.Marshal(v)
+	if err != nil {
+		// Swallow errors inside of a template.
+		return ""
+	}
+	return string(data)
+}
+
+// fromJSON converts a JSON document into a map[string]any.
+//
+// This is not a general-purpose JSON parser, and will not parse all valid
+// JSON documents. Additionally, because its intended use is within templates
+// it tolerates errors. It will insert the returned error message string into
+// m["Error"] in the returned map.
+func fromJSON(str string) map[string]any {
+	m := make(map[string]any)
+
+	if err := json.Unmarshal([]byte(str), &m); err != nil {
+		m["Error"] = err.Error()
+	}
+	return m
+}
+
+// fromJSONArray converts a JSON array into a []any.
+//
+// This is not a general-purpose JSON parser, and will not parse all valid
+// JSON documents. Additionally, because its intended use is within templates
+// it tolerates errors. It will insert the returned error message string as
+// the first and only item in the returned array.
+func fromJSONArray(str string) []any {
+	a := []any{}
+
+	if err := json.Unmarshal([]byte(str), &a); err != nil {
+		a = []any{err.Error()}
+	}
+	return a
+}
