e5cf3496648087a73cf33701cba43d35554953e3
Jim Ramsay <jramsay@redhat.com>
2025-01-06 18:26:07 -0500

    Add --version flag to cluster-compare and helpers
    Signed-off-by: Jim Ramsay <jramsay@redhat.com>

--- addon-tools/report-creator/report-creator.go
+++ addon-tools/report-creator/report-creator.go
@@ -7,8 +7,14 @@ import (
 	"github.com/openshift/kube-compare/addon-tools/report-creator/report"
 )
 
+var (
+	version = "unreleased"
+	date    = "unknown"
+)
+
 func main() {
 	cmd := report.NewCmd()
+	cmd.Version = fmt.Sprintf("%s (%s)", version, date)
 	if err := cmd.Execute(); err != nil {
 		_, _ = fmt.Fprintf(os.Stderr, "There was an error: '%s'", err)
 		os.Exit(1)
